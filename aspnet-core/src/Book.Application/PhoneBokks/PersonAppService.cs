01ab3ed563921d02eee042b2d7ce5a2658226edc
yangcs2016 <davidyangcs2012@gmail.com>
2019-03-20 22:09:13 +0800

    添加Application中的service接口类

---
+++ aspnet-core/src/Book.Application/PhoneBokks/PersonAppService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Book.PhoneBokks
+{
+    public class PersonAppService: BookAppServiceBase,IPersonAppService
+    {
+    }
+}
